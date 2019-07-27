0ce5e4cad09003f781e4461819d859e9908b5d6e
ghostcfl <946930866@qq.com>
2019-07-27 08:48:30 +0800

    fix

--- fix_data.py
+++ fix_data.py
@@ -5,11 +5,11 @@ from settings import SQL_SETTINGS
 app = Flask(__name__)
 
 
-@app.route('/')
-def result():
+@app.route('/<id>')
+def result(id):
     sql_ele = Sql(**SQL_SETTINGS)
-    res = sql_ele.select_data('tb_order_spider', 0, "*", orderNo='553392578891239204')
-    res2 = sql_ele.select_data('tb_order_detail_spider', 0, "*", orderNo='553392578891239204')
+    res = sql_ele.select_data('tb_order_spider', 0, "*", orderNo=id)
+    res2 = sql_ele.select_data('tb_order_detail_spider', 0, "*", orderNo=id)
     print(res)
     # dict = {'phy': 50, 'che': 60, 'maths': 70}
     return render_template('fix_data.html', result=res[0], item=res2)
@@ -17,15 +17,14 @@ def result():
 
 @app.route('/fix', methods=['POST', 'GET'])
 def fix():
+    sql_ele = Sql(**SQL_SETTINGS)
     if request.method == 'POST':
         # result = request.form
         result = request.form
         item = result.to_dict()
-        print(item)
-        couponPrice = item['couponPrice']
-        unitPrice = item['unitPrice']
-        print(couponPrice)
-        print(unitPrice)
+        sql_ele.update_old_data('tb_order_spider', {'couponPrice': item['couponPrice']}, {'orderNo': item['orderNo']})
+        sql_ele.update_old_data('tb_order_detail_spider', {'unitPrice': item['unitPrice']},
+                                {'orderNo': item['orderNo'], 'itemNo': item['itemNo']})
     return "Success"
 
 
