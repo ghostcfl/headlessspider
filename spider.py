b3d6fd1f42328bbde10084634648a230c551b4ff
ghostcfl <946930866@qq.com>
2019-08-02 15:18:16 +0800

    fix

--- spider.py
+++ spider.py
@@ -108,7 +108,7 @@ class Spider():
                 item['orderNo'] = mainOrders[i]["id"]
                 item['itemNo'] = j
                 item['goodsCode'] = items[j]['itemInfo']['extra'][0]['value']
-                item['tbName'] = re.sub("(\s+)", " ", items[j]['itemInfo']['title']).strip()
+                item['tbName'] = re.sub("(\s+)", " ", items[j]['itemInfo']['title']).strip().replace("&plusmn;", "±")
                 item['unitPrice'] = items[j]['priceInfo']['realTotal']
                 item['sellNum'] = items[j]['quantity']
                 item['orderStatus'] = order['orderStatus']
